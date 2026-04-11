15658f750c74ab89e0571d594a0a250030fd03ce
bhanu.srikakulapu.contractor@pepsico.com <bhanu.srikakulapu.contractor@pepsico.com>
2026-04-11 08:28:57 +0530

    crm also completed where need to test them from gst_registration and check these here appearing or not and then crm funtionalities and remove follow_ups at service level for gst_registrations and added into crm...

--- docs/62-crm-leads-last-connected-at.sql
+++
@@ -1,8 +0,0 @@
--- Add last_connected_at to CRM leads
-ALTER TABLE solvetax.crm_leads
-ADD COLUMN IF NOT EXISTS last_connected_at timestamptz NULL;
-
--- Optional helper index for recent connected activity queries
-CREATE INDEX IF NOT EXISTS idx_crm_leads_last_connected_at
-ON solvetax.crm_leads (last_connected_at)
-WHERE is_active = TRUE;
